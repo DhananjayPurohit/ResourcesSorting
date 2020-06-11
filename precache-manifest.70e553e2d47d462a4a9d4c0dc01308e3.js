cc75ad7801efbaffc41572f6fb82cc7fc67ea6e4
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-06-11 12:03:18 +0530

    Updates

---
+++ precache-manifest.70e553e2d47d462a4a9d4c0dc01308e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a54f420f01cf1f84aa1b15ad913361",
+    "url": "/ResourcesSorting/index.html"
+  },
+  {
+    "revision": "154b42c12c7326921399",
+    "url": "/ResourcesSorting/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cec4fc1c68a0341334f1",
+    "url": "/ResourcesSorting/static/js/2.c0904202.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ResourcesSorting/static/js/2.c0904202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154b42c12c7326921399",
+    "url": "/ResourcesSorting/static/js/main.4cf702ed.chunk.js"
+  },
+  {
+    "revision": "2a9c4a3d06e777731033",
+    "url": "/ResourcesSorting/static/js/runtime-main.fdcbb926.js"
+  }
+]);
